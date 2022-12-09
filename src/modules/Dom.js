23051c38933a468ee972ec68ec8d17c0683aac98
Andrei Dyminski <smoke59@mail.ru>
2022-12-09 20:43:46 +0300

    added class Dom

---
+++ src/modules/Dom.js
@@ -0,0 +1,21 @@
+export default class Dom {
+	setValue($el, value) {
+		$el.innerText = value
+	}
+
+	resetValue($el) {
+		$el.innerText = '0'
+	}
+
+	setProgress($el, value) {
+		$el.style.width = value * 100 + '%'
+	}
+
+	showElement($el) {
+		$el.style.display = 'block'
+	}
+
+	hideElement($el) {
+		$el.style.display = 'none'
+	}
+}
