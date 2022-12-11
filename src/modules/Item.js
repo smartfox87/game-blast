f25ef5a0790de2e54d9e0c1d1568f8feb0da4800
Andrei Dyminski <smoke59@mail.ru>
2022-12-11 12:31:19 +0300

    transformed private fields of Item to public

--- src/modules/Item.js
+++ src/modules/Item.js
@@ -1,36 +1,16 @@
 export default class Item {
-	#chance
-	#type
-	#image
-	#width
-	#height
+	chance
+	type
+	image
+	width
+	height
 
 	constructor(chance, type, src, width, height) {
-		this.#chance = chance
-		this.#type = type
-		this.#image = new Image()
-		this.#image.src = src
-		this.#image.width = this.#width = width
-		this.#image.height = this.#height = height
-	}
-
-	get chance() {
-		return this.#chance
-	}
-
-	get image() {
-		return this.#image
-	}
-
-	get type() {
-		return this.#type
-	}
-
-	get width() {
-		return this.#width
-	}
-
-	get height() {
-		return this.#height
+		this.chance = chance
+		this.type = type
+		this.image = new Image()
+		this.image.src = src
+		this.image.width = this.width = width
+		this.image.height = this.height = height
 	}
 }
