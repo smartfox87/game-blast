a7b12b589e7eff838f050c19123e49f8d311263c
Andrei Dyminski <smoke59@mail.ru>
2022-12-11 19:00:38 +0300

    updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: {
@@ -9,6 +10,7 @@ module.exports = {
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: 'assets/[name][ext][query]',
 		clean: true,
 	},
 	// devtool: 'inline-source-map',
@@ -33,48 +35,38 @@ module.exports = {
 				}
 			},
 			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				type: 'asset/resource'
+			},
+			{
+				test: /\.(eot|woff|woff2|svg|ttf|otf)([\?]?.*)$/,
+				type: 'asset/resource'
+			},
+			{
 				test: /\.s[ac]ss$/i,
 				use: [
 					// Creates `style` nodes from JS strings
-					'style-loader',
+					MiniCssExtractPlugin.loader,
 					// Translates CSS into CommonJS
 					'css-loader',
 					// Compiles Sass to CSS
 					'sass-loader',
 				],
 			},
-			// {
-			// 	test: /\.(png|jpg|gif)$/i,
-			// 	use: [
-			// 		{
-			// 			loader: 'url-loader',
-			// 			options: {
-			// 				limit: false,
-			// 			},
-			// 		},
-			// 	],
-			// },
-			// {
-			// 	test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
-			// 	use: {
-			// 		loader: 'file-loader',
-			// 		options: {
-			// 			name: '[name].[ext]',
-			// 			outputPath: 'fonts',
-			// 			publicPath: '../fonts',
-			// 		}
-			// 	}
-			// },
 		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: 'Hot Module Replacement',
 			template: 'src/index.html'
+		}),
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: 'css/style.css',
 		}),
 		new CopyPlugin({
 			patterns: [
-				{from: './src/assets/images', to: 'images'},
+				{from: './src/assets/images/items', to: 'assets/items'},
 			]
 		}),
 	],
