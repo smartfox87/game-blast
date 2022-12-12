df68c53fb4b4f320489f9d3d565e092e1539d111
Andrei Dyminski <smoke59@mail.ru>
2022-12-12 18:39:13 +0300

    added animation to setting values

--- src/modules/Dom.js
+++ src/modules/Dom.js
@@ -1,10 +1,10 @@
 export default class Dom {
 	setValue($el, value) {
-		$el.innerText = value
+		this.animateValue($el, $el.innerText, value)
 	}
 
 	resetValue($el) {
-		$el.innerText = '0'
+		this.animateValue($el, $el.innerText, 0)
 	}
 
 	setWidth($el, value) {
@@ -17,5 +17,19 @@ export default class Dom {
 
 	hideElement($el) {
 		$el.style.display = 'none'
+	}
+
+	animateValue(obj, start = 0, end) {
+		let startTimestamp = null;
+		const duration = (Math.abs(end - start) || 1) * 25
+		const step = (timestamp) => {
+			if (!startTimestamp) startTimestamp = timestamp;
+			const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+			obj.innerText = Math.floor(progress * (end - start) + parseFloat(start));
+			if (progress < 1) {
+				requestAnimationFrame(step);
+			}
+		};
+		requestAnimationFrame(step);
 	}
 }
